2b89c027ade46ebffb940f7e58a11a40245e7e0f
Asmit Aditya Singh <asmitadsingh13@gamil.com>
2025-02-13 00:37:33 +0530

    upddated cors policy

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -4,8 +4,8 @@ import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
 
-const socket = io(`${import.meta.env.VITE_BASE_URL}`); // Replace with your server URL
-
+const socket = io(`${import.meta.env.VITE_BASE_URL}` , { withCredentials: true,
+    transports: ["websocket", "polling"], }); // Replace with your server URL
 const SocketProvider = ({ children }) => {
     useEffect(() => {
         // Basic connection logic
